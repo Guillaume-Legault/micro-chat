2487d35ac95f17f95556dee376539b7d4d7ab41e
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-10-27 13:09:14 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 9) {
-        basic.showNumber(3)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.pause(500)
-        basic.showNumber(2)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.pause(500)
-        basic.showNumber(1)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.pause(200)
-        basic.showString("Go")
-        music.playTone(349, music.beat(BeatFraction.Double))
+        for (let index = 0; index < 1; index++) {
+            basic.showNumber(3)
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            basic.pause(500)
+            basic.showNumber(2)
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            basic.pause(500)
+            basic.showNumber(1)
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            basic.pause(200)
+            basic.showString("Go")
+            music.playTone(349, music.beat(BeatFraction.Double))
+        }
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -38,4 +40,5 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
 })
 basic.clearScreen()
+music.setVolume(62)
 radio.setGroup(134)
